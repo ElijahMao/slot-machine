844390d7ba45c4d44a562b256fe697fe3137fcb5
ElijahMao <40432292+ElijahMao@users.noreply.github.com>
2018-08-25 11:41:32 +0800

    20180820

---
+++ Form1.Designer.vb
@@ -0,0 +1,191 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請不要使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btn_reset = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.lbl_state = New System.Windows.Forms.Label()
+        Me.btn_AllIn = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button1 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(78, 256)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(56, 16)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "投注量"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(78, 304)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(56, 16)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "總資產"
+        '
+        'btn_reset
+        '
+        Me.btn_reset.Location = New System.Drawing.Point(287, 299)
+        Me.btn_reset.Name = "btn_reset"
+        Me.btn_reset.Size = New System.Drawing.Size(75, 27)
+        Me.btn_reset.TabIndex = 3
+        Me.btn_reset.Text = "重來"
+        Me.btn_reset.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(34, 80)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(100, 50)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 4
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(170, 80)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(100, 50)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 5
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Image = CType(resources.GetObject("PictureBox3.Image"), System.Drawing.Image)
+        Me.PictureBox3.Location = New System.Drawing.Point(305, 80)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(100, 50)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox3.TabIndex = 6
+        Me.PictureBox3.TabStop = False
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Increment = New Decimal(New Integer() {100, 0, 0, 0})
+        Me.NumericUpDown1.Location = New System.Drawing.Point(140, 251)
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(120, 27)
+        Me.NumericUpDown1.TabIndex = 7
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(140, 299)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(120, 27)
+        Me.TextBox1.TabIndex = 8
+        '
+        'lbl_state
+        '
+        Me.lbl_state.Font = New System.Drawing.Font("新細明體", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.lbl_state.Location = New System.Drawing.Point(34, 26)
+        Me.lbl_state.Name = "lbl_state"
+        Me.lbl_state.Size = New System.Drawing.Size(371, 27)
+        Me.lbl_state.TabIndex = 9
+        Me.lbl_state.Text = "請按啟動開始遊戲"
+        Me.lbl_state.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'btn_AllIn
+        '
+        Me.btn_AllIn.Location = New System.Drawing.Point(287, 251)
+        Me.btn_AllIn.Name = "btn_AllIn"
+        Me.btn_AllIn.Size = New System.Drawing.Size(75, 27)
+        Me.btn_AllIn.TabIndex = 10
+        Me.btn_AllIn.Text = "梭哈"
+        Me.btn_AllIn.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("華康雅風體W3", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Button1.Location = New System.Drawing.Point(153, 158)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(136, 60)
+        Me.Button1.TabIndex = 12
+        Me.Button1.Text = "啟動"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(437, 349)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.btn_AllIn)
+        Me.Controls.Add(Me.lbl_state)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.btn_reset)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("新細明體", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "Form1"
+        Me.Text = "吃角子老虎"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents btn_reset As System.Windows.Forms.Button
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox3 As System.Windows.Forms.PictureBox
+    Friend WithEvents NumericUpDown1 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents lbl_state As System.Windows.Forms.Label
+    Friend WithEvents btn_AllIn As System.Windows.Forms.Button
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,133 @@
+﻿Public Class Form1
+
+    Dim photo() As String = {"7", "企鵝", "沙漠", "無尾熊", "菊花", "鬱金香", "繡球花", "水母", "燈塔"}
+    Dim n1, n2, n3 As Integer, total As Integer = 1000
+    Dim r As New Random()
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        TextBox1.Text = total.ToString()
+        NumericUpDown1.Maximum = total
+        NumericUpDown1.Value = "100"
+        btn_reset.Enabled = False
+    End Sub
+
+    Private Sub btn_AllIn_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn_AllIn.Click
+        NumericUpDown1.Value = total
+    End Sub
+
+    Private Sub btn_reset_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn_reset.Click
+        PictureBox1.Image = New Bitmap("7.png")
+        PictureBox2.Image = New Bitmap("7.png")
+        PictureBox3.Image = New Bitmap("7.png")
+        total = 1000
+        TextBox1.Text = total.ToString()
+        NumericUpDown1.Maximum = total
+        NumericUpDown1.Value = "100"
+        NumericUpDown1.Enabled = True
+        Button1.Enabled = True
+        btn_reset.Enabled = False
+        btn_AllIn.Enabled = True
+        lbl_state.Text = "請按啟動開始遊戲"
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        If Timer1.Enabled = False Then
+            If NumericUpDown1.Value = 0 Then
+                MessageBox.Show("請選擇投注量")
+            Else
+                lbl_state.Text = ""
+                Timer1.Interval = 1
+                Timer1.Enabled = True
+                Button1.Text = "停止"
+                btn_reset.Enabled = False
+                btn_AllIn.Enabled = False
+                NumericUpDown1.Enabled = False
+            End If
+        ElseIf Timer1.Enabled = True Then
+            Timer1.Enabled = False
+            btn_AllIn.Enabled = True
+            NumericUpDown1.Enabled = True
+
+            If n1 = n2 Then
+                If n2 = n3 Then
+                    If n1 = 0 Then
+                        lbl_state.ForeColor = Color.Red
+                        lbl_state.Text = "恭喜中頭獎，獲得100倍獎金!!!"
+                        total += NumericUpDown1.Value * 100
+                    Else
+                        lbl_state.ForeColor = Color.Red
+                        lbl_state.Text = "恭喜中特獎，獲得10倍獎金!!!"
+                        total += NumericUpDown1.Value * 10
+                    End If
+                Else
+                    If n1 = 0 Then
+                        lbl_state.ForeColor = Color.Red
+                        lbl_state.Text = "恭喜中獎，獲得5倍獎金"
+                        total += NumericUpDown1.Value * 5
+                    Else
+                        lbl_state.ForeColor = Color.Brown
+                        lbl_state.Text = "恭喜中獎"
+                        total += NumericUpDown1.Value
+                    End If
+                End If
+            Else
+                If n2 = n3 Then
+                    If n2 = 0 Then
+                        lbl_state.ForeColor = Color.Red
+                        lbl_state.Text = "恭喜中獎，獲得5倍獎金"
+                        total += NumericUpDown1.Value * 5
+                    Else
+                        lbl_state.ForeColor = Color.Brown
+                        lbl_state.Text = "恭喜中獎"
+                        total += NumericUpDown1.Value
+                    End If
+                ElseIf n1 = n3 Then
+                    If n1 = 0 Then
+                        lbl_state.ForeColor = Color.Red
+                        lbl_state.Text = "恭喜中獎，獲得5倍獎金"
+                        total += NumericUpDown1.Value * 5
+                    Else
+                        lbl_state.ForeColor = Color.Brown
+                        lbl_state.Text = "恭喜中獎"
+                        total += NumericUpDown1.Value
+                    End If
+                Else
+                    If n1 = 0 Or n2 = 0 Or n3 = 0 Then
+                        lbl_state.ForeColor = Color.Red
+                        lbl_state.Text = "恭喜中獎，獲得3倍獎金"
+                        total += NumericUpDown1.Value * 3
+                    Else
+                        total -= NumericUpDown1.Value
+                        lbl_state.ForeColor = Color.Black
+                        lbl_state.Text = "真可惜"
+                    End If
+                End If
+            End If
+
+            Button1.Text = "啟動"
+            TextBox1.Text = total.ToString()
+            NumericUpDown1.Maximum = total
+            If total <= 0 Then
+                MessageBox.Show("破產了QQ")
+                NumericUpDown1.Value = 0
+                NumericUpDown1.Enabled = False
+                Button1.Enabled = False
+                btn_reset.Enabled = True
+                btn_AllIn.Enabled = False
+                lbl_state.Text = "請按重來重新遊戲"
+            End If
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        n1 = r.Next(0, 8)
+        PictureBox1.Image = New Bitmap(photo(n1).ToString() & ".jpg")
+
+        n2 = r.Next(0, 8)
+        PictureBox2.Image = New Bitmap(photo(n2).ToString() & ".jpg")
+
+        n3 = r.Next(0, 8)
+        PictureBox3.Image = New Bitmap(photo(n3).ToString() & ".jpg")
+    End Sub
+
+End Class
